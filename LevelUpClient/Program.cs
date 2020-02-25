798deb5a2064f8f7940c2811846385cf149dae71
Doomky <james.richet.pro@gmail.com>
2020-02-25 15:07:01 +0100

    [LevelUpClient] basic Client Console Application created

---
+++ LevelUpClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelUpClient
+{
+    class Program
+    {
+
+        public static async Task Main(string address = "https://localhost", string port = "5001")
+        {
+
+        }
+    }
+}
