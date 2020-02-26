c0721a92a1df697a56da033730c43a2430abf888
Allan Cantin <allan.cantin@epita.fr>
2020-02-26 12:52:29 +0100

    [LevelUpRequests] add Request base class (empty for now) + SignIn request

---
+++ LevelUpRequests/Request.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LevelUpRequests
+{
+    abstract class Request
+    {
+        
+    }
+}
---
+++ LevelUpRequests/SignInRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LevelUpRequests
+{
+    class SignInRequest
+    {
+        private string _login;
+        private string _emailAddress;
+        private string _hashPassword;
+    }
+}
