a61dbf1edd079662833d38c9206cd0151f10a230
Allan Cantin <allan.cantin@epita.fr>
2020-02-26 14:10:20 +0100

    [Request] add SignUpRequest class with some properties

---
+++ LevelUpRequests/SignUpRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LevelUpRequests
+{
+    public class SignUpRequest : Request
+    {
+        public string Login { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string EmailAddress { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
