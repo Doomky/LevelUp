8c2691f7dfdf25a277dde1727a1a2d1411599c76
Allan Cantin <allan.cantin@epita.fr>
2020-02-27 15:22:12 +0100

    [Request] add behavior to SignIn Request Handler

--- LevelUpAPI/RequestHandlers/SignInRequestHandler.cs
+++ LevelUpAPI/RequestHandlers/SignInRequestHandler.cs
@@ -1,11 +1,11 @@
-﻿using LevelUpRequests;
+﻿using LevelUpAPI.Model;
+using LevelUpRequests;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
+using System.Linq;
 
 namespace LevelUpAPI
 {
@@ -13,7 +13,25 @@ namespace LevelUpAPI
     {
         protected override async void ExecuteRequest(HttpContext context)
         {
-            context.Response.StatusCode = StatusCodes.Status200OK;
+            if (Request == null)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            using (var dbcontext = new levelupContext())
+            {
+                var query = from users in dbcontext.Users
+                            where users.Login == Request.Login || users.Email == Request.EmailAddress
+                            select users;
+                if (query.Any())
+                {
+                    context.Response.StatusCode = StatusCodes.Status200OK;
+                    return;
+                }
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
         }
     }
 }
