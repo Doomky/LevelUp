c8555f42787fad1036a94d3f9b9b71c0e543c31e
Doomky <james.richet@epita.fr>
2020-03-20 14:17:58 +0100

    [Client/UpdateFoodEntryRequest] builder added

---
+++ LevelUpClient/RequestBuilders/ConsoleUpdateFoodEntryRequestBuilder.cs
@@ -0,0 +1,34 @@
+﻿using LevelUpRequests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LevelUpClient.RequestBuilders
+{
+    public class ConsoleUpdateFoodEntryRequestBuilder : RequestBuilder<UpdateFoodEntryRequest>
+    {
+        public ConsoleUpdateFoodEntryRequestBuilder WithId()
+        {
+            Console.Write("Id: ");
+            if (int.TryParse(Console.ReadLine(), out int userId))
+                Request.Id = userId;
+            return this;
+        }
+
+        public ConsoleUpdateFoodEntryRequestBuilder WithUserId()
+        {
+            Console.Write("User Id: ");
+            if (int.TryParse(Console.ReadLine(), out int userId))
+                Request.UserId = userId;
+            return this;
+        }
+
+        public ConsoleUpdateFoodEntryRequestBuilder WithOFFDataId()
+        {
+            Console.Write("OFF Data Id: ");
+            if (int.TryParse(Console.ReadLine(), out int offDataId))
+                Request.OFFDataId = offDataId;
+            return this;
+        }
+    }
+}
