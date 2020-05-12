311f53df8a3480817435fff34189ae2a82ec55d2
Allan Cantin <allan.cantin@epita.fr>
2020-05-12 17:55:47 +0200

    [API] added physical activities controller with the first endpoint: get physical activity entries

---
+++ LevelUpAPI/Controllers/PhysicalActivitiesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LevelUpAPI.DataAccess.Repositories.Interfaces;
+using LevelUpAPI.RequestHandlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LevelUpAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PhysicalActivitiesController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPhysicalActivitiesEntryRepository _physicalActivitiesEntryRepository;
+
+        public PhysicalActivitiesController(IUserRepository userRepository, IPhysicalActivitiesEntryRepository physicalActivitiesEntryRepository)
+        {
+            _userRepository = userRepository;
+            _physicalActivitiesEntryRepository = physicalActivitiesEntryRepository;
+        }
+
+        /// <summary>
+        /// Get all the physical activity entries of the signed-in user. 
+        /// </summary>
+        /// <response code="200">The physical activities were found.</response>
+        /// <response code="400">The request is malformed or the user does not exist.</response>
+        /// <response code="401">The user is not signed in.</response>
+        [HttpGet]
+        public void Get()
+        {
+            GetPAEntriesRequestHandler getPAEntriesRequestHandler = new GetPAEntriesRequestHandler(_userRepository, _physicalActivitiesEntryRepository);
+            getPAEntriesRequestHandler.Execute(HttpContext);
+        }
+    }
+}
---
+++ LevelUpAPI/RequestHandlers/GetPAEntriesRequestHandler.cs
@@ -0,0 +1,63 @@
+﻿using LevelUpAPI.DataAccess.Repositories.Interfaces;
+using LevelUpAPI.Dbo;
+using LevelUpRequests;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LevelUpAPI.RequestHandlers
+{
+    public class GetPAEntriesRequestHandler : RequestHandler<GetPAEntriesRequest>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPhysicalActivitiesEntryRepository _physicalActivitiesEntryRepository;
+
+        public GetPAEntriesRequestHandler(IUserRepository userRepository, IPhysicalActivitiesEntryRepository physicalActivitiesEntryRepository)
+        {
+            _userRepository = userRepository;
+            _physicalActivitiesEntryRepository = physicalActivitiesEntryRepository;
+        }
+
+        protected override void ExecuteRequest(HttpContext context)
+        {
+            if (Request == null)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            ClaimsPrincipal claims = context.User;
+
+            if (claims == null)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                context.Response.WriteAsync("no claims").GetAwaiter().GetResult();
+                return;
+            }
+
+            User user = _userRepository.GetUserByClaims(claims).GetAwaiter().GetResult();
+
+            if (user == null)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                context.Response.WriteAsync("no user for this client_id").GetAwaiter().GetResult();
+                return;
+            }
+
+            List<NbPhysicalActivitiesEntriesByLogin> PAEntries = _physicalActivitiesEntryRepository.GetNbPhysicalActivitiesEntries(user.Login);
+
+            if (PAEntries != null)
+            {
+                string PAEntriesJson = JsonSerializer.Serialize(PAEntries);
+                context.Response.StatusCode = StatusCodes.Status200OK;
+                context.Response.WriteAsync(PAEntriesJson).GetAwaiter().GetResult();
+            }
+            else
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+}
