5dd6cb66b71037e7443f29acf845e7a5e263fdc0
Allan Cantin <allan.cantin@epita.fr>
2020-09-12 18:25:31 +0200

    [API/Migrations] added migrations for skins table and the skins data

--- LevelUpAPI/Migrations/20200912161955_Skin_Data.Designer.cs
+++ LevelUpAPI/Migrations/20200912161955_Skin_Data.Designer.cs
@@ -4,14 +4,16 @@ using LevelUpAPI.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LevelUpAPI.Migrations
 {
     [DbContext(typeof(levelupContext))]
-    partial class levelupContextModelSnapshot : ModelSnapshot
+    [Migration("20200912161955_Skin_Data")]
+    partial class Skin_Data
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LevelUpAPI/Migrations/20200912161955_Skin_Data.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LevelUpAPI.Migrations
+{
+    public partial class Skin_Data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+@"INSERT INTO [dbo].[skins]
+            ([name], [level_min])
+        VALUES
+            ('man_default', 0),
+            ('woman_default', 0),
+            ('non-binary_default', 0)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+@"DELETE FROM [dbo].[skins]");
+        }
+    }
+}
